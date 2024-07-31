28121ed978226d7e466d6a78ccd59131b7577dbd
Omkar-Abhang <omkarabhang36@gmail.com>
2024-07-31 12:38:40 +0530

    todo form that have functionality to add task and remove task.

---
+++ src/components/todoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ''
+
+export const todoform = () => {
+    return (
+        <div>
+            <form action="">
+                <title>Add Today's Task!</title>
+                <input type="text" placeholder='Add Task'/>
+                <button type='submit'>Add Task</button>
+            </form>
+        </div>
+    )
+}
